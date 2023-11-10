c60b2aece23c0bbbef12e177a18e7da97cfa8aa9
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-11-10 15:55:18 -0600

    [ADD] alerts to Login

--- FrontendZenPulse/src/components/atoms/forms/LoginForm.jsx
+++ FrontendZenPulse/src/components/atoms/forms/LoginForm.jsx
@@ -1,27 +1,21 @@
-import { useRef } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 function LoginForm() {
-  // Create a reference for the form
   const formRef = useRef();
-  // Use the useNavigate hook from React Router
   const navigate = useNavigate();
 
-  // Function to handle form submission
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
-    // Create a FormData object from the form
     const formLogin = new FormData(formRef.current);
-
-    // Extract email and password from form data
     const data = {
       email: formLogin.get('email'),
       password: formLogin.get('password')
     };
 
     try {
-      // Send a POST request to the login endpoint
       const response = await fetch('http://localhost:4000/login', {
         method: 'POST',
         headers: {
@@ -30,19 +24,30 @@ function LoginForm() {
         body: JSON.stringify(data)
       });
 
-      // Check if the response is OK
       if (!response.ok) {
+        // Mostrar mensaje de error con SweetAlert2
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Usuario o contraseña incorrectos',
+        });
         throw new Error('Network response was not ok');
       }
 
-      // Show a success alert and navigate to the dashboard
-      //alert('Session started successfully');
-      navigate("/dashboard");
+      // Mostrar mensaje de éxito con SweetAlert2
+      Swal.fire({
+        icon: 'success',
+        title: '¡Éxito!',
+        text: 'Sesión iniciada correctamente',
+      });
+
+      // Si todo está bien, navegar a la página de dashboard
+      navigate('/dashboard');
     } catch (error) {
-      // Log an error if there's a problem with the fetch operation
+      // Loguear error en la consola
       console.error('There has been a problem with your fetch operation:', error);
     }
-  }
+  };
 
   return (
     <>
@@ -87,10 +92,7 @@ function LoginForm() {
               <button
                 className='flex items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform py-4  rounded-xl text-gray-700 font-semibold text-lg border-2 border-gray-100 '>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5.26644 9.76453C6.19903 6.93863 8.85469 4.90909 12.0002 4.90909C13.6912 4.90909 15.2184 5.50909 16.4184 6.49091L19.9093 3C17.7821 1.14545 15.0548 0 12.0002 0C7.27031 0 3.19799 2.6983 1.24023 6.65002L5.26644 9.76453Z" fill="#EA4335" />
-                  <path d="M16.0406 18.0142C14.9508 18.718 13.5659 19.0926 11.9998 19.0926C8.86633 19.0926 6.21896 17.0785 5.27682 14.2695L1.2373 17.3366C3.19263 21.2953 7.26484 24.0017 11.9998 24.0017C14.9327 24.0017 17.7352 22.959 19.834 21.0012L16.0406 18.0142Z" fill="#34A853" />
-                  <path d="M19.8342 20.9978C22.0292 18.9503 23.4545 15.9019 23.4545 11.9982C23.4545 11.2891 23.3455 10.5255 23.1818 9.81641H12V14.4528H18.4364C18.1188 16.0119 17.2663 17.2194 16.0407 18.0108L19.8342 20.9978Z" fill="#4A90E2" />
-                  <path d="M5.27698 14.2663C5.03833 13.5547 4.90909 12.7922 4.90909 11.9984C4.90909 11.2167 5.03444 10.4652 5.2662 9.76294L1.23999 6.64844C0.436587 8.25884 0 10.0738 0 11.9984C0 13.918 0.444781 15.7286 1.23746 17.3334L5.27698 14.2663Z" fill="#FBBC05" />
+                  {/* Icono de Google */}
                 </svg>
                 Sign in with Google
               </button>
