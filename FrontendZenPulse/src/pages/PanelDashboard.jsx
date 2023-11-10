f247b4a297c24affa5a46467caa25859d15abb94
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-11-10 14:21:35 -0600

    [ADD] page PanelDashboard

---
+++ FrontendZenPulse/src/pages/PanelDashboard.jsx
@@ -0,0 +1,10 @@
+import PanelDashboardContainer from "../containers/PanelDashboardContainer";
+
+function PanelDashboard(){
+
+    return(
+        <PanelDashboardContainer/>
+    )
+}
+
+export default PanelDashboard;
